a3ac14967f3411ce37dd5bf09ae6fddd1631a6ee
Majid Kamali <majidkamali1370@gmail.com>
2025-06-21 18:20:32 +0100

    Add comments to formatting example

--- 02_formatting/src/main.rs
+++ 02_formatting/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     let c = a / b;
     let my_string = "Hello world";
 
-    println!("c is {:.3}", c);
-    println!("c is {:8.3}", c);
-    println!("c is {:+8.3}", c);
-    println!("c is {:+08.3}", c);
-    println!("a is {2}, b is {1} c is {0:.3}", c, b, a);
-    println!("Sentence is '{my_string}'");
+    println!("c is {:.3}", c); // Show 3 digits after decimal point
+    println!("c is {:8.3}", c); // Print the value using width of 8 characters
+    println!("c is {:+8.3}", c); // Shw plus sign
+    println!("c is {:+08.3}", c); // Fill left with zero
+    println!("a is {2}, b is {1} c is {0:.3}", c, b, a); // Reorder variables
+    println!("Sentence is '{my_string}'"); // Can use variable name inside string
 }
