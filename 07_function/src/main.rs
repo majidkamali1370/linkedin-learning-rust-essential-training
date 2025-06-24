9b977fd459a55b8c0e2b3cc54ca30c907544fe6e
Majid Kamali <majidkamali1370@gmail.com>
2025-06-24 17:26:24 +0100

    Add comments to function example

--- 07_function/src/main.rs
+++ 07_function/src/main.rs
@@ -8,18 +8,22 @@ fn main() {
     say_a_number(return_subtract_as_i32(100, 125));
 }
 
+// Unlike C/C++, functions can be defined after main function
 fn say_hello() {
     println!("Hello :)");
 }
 
+// Pass a 32-bit integer by value
 fn say_a_number(number: i32) {
     println!("Number is {}", number);
 }
 
+// Pass a string slice by reference (borrowed)
 fn say_a_string(s: &str) {
     println!("String is '{}'", s);
 }
 
+// Return type is shown by an arrow at the end
 fn return_subtract_as_i32(a: u8, b: u8) -> i32 {
     return a as i32 - b as i32;
 }
