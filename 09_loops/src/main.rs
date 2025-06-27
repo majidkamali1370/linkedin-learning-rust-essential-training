9c762c133e99c24e66013679cdac0ce46b657a10
Majid Kamali <majidkamali1370@gmail.com>
2025-06-27 17:45:34 +0100

    Add comments to loops example

--- 09_loops/src/main.rs
+++ 09_loops/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut count = 0;
 
+    // Infinite loop, must be explicitly break (optionally with a value)
     let result = loop {
         count += 1;
         println!("LOOP => count is {count}");
@@ -12,17 +13,21 @@ fn main() {
 
     println!("result = {result}");
 
+    // While loop, must be explicitly break
     while count <= 20 {
         println!("WHILE => count is {count}");
-        count += 1;
+        count += 1; // There's no ++ operator in Rust
     }
 
     let items = [1, 2, 3, 4, 5];
 
+    // For loop, for each item in collection
     for item in items {
         println!("FOR => item is {item}");
     }
 
+    // For loop, for each item in range
+    // Use .. to represent a range of numbers. Operands can be either literals or variables
     for number in 0..10 {
         println!("number is {number}");
     }
