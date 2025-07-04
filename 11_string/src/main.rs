cbbd4c35c1cbd43e521e77e6820b1eadb1e12f8f
Majid Kamali <majidkamali1370@gmail.com>
2025-07-04 18:44:27 +0100

    Add comments to string example

--- 11_string/src/main.rs
+++ 11_string/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut my_string = String::from("Hello");
+    let mut my_string = String::from("Hello"); // Create String variable from slice
     println!("{my_string}");
-    my_string.push_str(" World!");
+    my_string.push_str(" World!"); // Append string slice
     println!("{my_string}");
 }
