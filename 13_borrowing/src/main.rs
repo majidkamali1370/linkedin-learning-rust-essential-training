ef55780c1e4f97c7d23837500be0a58662170812
Majid Kamali <majidkamali1370@gmail.com>
2025-07-09 16:45:21 +0100

    Add comments to borrowing example

--- 13_borrowing/src/main.rs
+++ 13_borrowing/src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
     let mut s = String::from("Hello");
 
-    borrow_string(&s);
+    borrow_string(&s); // Borrow const string to function
     println!("s = {s}");
 
-    borrow_and_modify_string(&mut s);
+    borrow_and_modify_string(&mut s); // Borrow mutable string to function
     println!("s = {s}");
 }
 
+// Function signature for const borrowed string
 fn borrow_string(s: &String) {
     println!("s.len = {}", s.len());
 }
 
+// Function signature for mutable borrowed string
 fn borrow_and_modify_string(s: &mut String) {
     s.push_str(", this is a suffix");
 }
