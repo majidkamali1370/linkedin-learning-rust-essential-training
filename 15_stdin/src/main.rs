f1bd61aea0197a985ea4551665f331bd641e9717
Majid Kamali <majidkamali1370@gmail.com>
2025-07-11 18:53:26 +0100

    Add comments to stdin example

--- 15_stdin/src/main.rs
+++ 15_stdin/src/main.rs
@@ -3,6 +3,9 @@ use std::io;
 fn main() {
     let mut name = String::new();
     println!("Enter your name: ");
+
+    // read_line function returns a Result type
+    // resulting string also contains the trailing new line
     let read_result = io::stdin().read_line(&mut name);
     println!("Hello {name}!");
     println!("Read {} characters from stdin", read_result.unwrap());
